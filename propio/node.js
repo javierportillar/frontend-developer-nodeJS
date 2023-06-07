39c0eeb3c5c8ccf3de7504e1f6b24abb35c81324
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-06-07 17:53:09 -0500

    nav modificado con menu

---
+++ propio/node.js
@@ -0,0 +1,9 @@
+var mail = document.querySelector('.navbar-email');
+var menuMail = document.querySelector('.desktop-menu');
+mail.addEventListener('click', aparydesap);
+
+ function aparydesap(){ 
+    menuMail.classList.toggle('inactive');
+    console.log("eliminado");
+}
+    
