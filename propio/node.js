d51a8af8e98208fcea7627be5419b4dab59b4168
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-06-07 18:22:13 -0500

    Se agregó la opción de desplegar menu en mobile, falta hacrlo responsive para que no salga

--- propio/node.js
+++ propio/node.js
@@ -1,9 +1,17 @@
 var mail = document.querySelector('.navbar-email');
 var menuMail = document.querySelector('.desktop-menu');
-mail.addEventListener('click', aparydesap);
+var menu = document.querySelector('.menu');
+var menuMobile = document.querySelector('.mobile-menu');
 
- function aparydesap(){ 
+mail.addEventListener('click', aparydesapd);
+menu.addEventListener('click', aparydesapm);
+
+ function aparydesapd(){ 
     menuMail.classList.toggle('inactive');
     console.log("eliminado");
 }
     
+function aparydesapm(){ 
+    menuMobile.classList.toggle('inactive');
+    console.log("eliminado mobile");
+}
