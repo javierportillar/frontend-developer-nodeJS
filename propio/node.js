5c0c036e72d713cea4860a32d236e1cfeb90e68c
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-06-09 14:39:41 -0500

    Se puso la lista de elementos pero en java

--- propio/node.js
+++ propio/node.js
@@ -5,6 +5,25 @@ var menuMobile = document.querySelector('.mobile-menu');
 var carrito = document.querySelector('.navbar-shopping-cart');
 var carrdet = document.querySelector('.product-detail');
 
+// CONTENEDOR DE ELEMENTOS
+var contenedor = document.querySelector('.cards-container');
+
+for (let i = 0; i < 7; i++) {   
+    contenedor.innerHTML+= 
+    ` <div class="product-card ${i}">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>`;
+}
+
 mail.addEventListener('click', aparydesapd);
 menu.addEventListener('click', aparydesapm);
 carrito.addEventListener('click', aparydesapcar);
